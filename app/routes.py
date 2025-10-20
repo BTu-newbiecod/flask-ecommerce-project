5d1c4e4ac3b905ae87c4965bfd21474505a11ab1
Metal Lotus <165745284+MetalThong@users.noreply.github.com>
2025-10-20 23:42:07 +0700

    Dựng trang chủ và Hiển thị sản phẩm

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 
 from flask import Blueprint, render_template
-
+from app.models import Product
 
 bp = Blueprint('main', __name__)
 
@@ -8,4 +8,10 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 @bp.route('/index')
 def index():
-    return render_template("index.html")
+    products = Product.query.all()
+    return render_template("index.html", products = products)
+
+@bp.route('/product/<int:id>')
+def product_detail(id):
+    product = Product.query.get_or_404(id)
+    return render_template('product_detail.html', product=product)
