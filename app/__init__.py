abc559c7bb63b26a333586403712b1416f9b64ad
Batu <tn150938@gmail.com>
2025-10-21 15:06:47 +0700

    hoàn thanh task 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, app
 from config import Config #import class/ham
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 #khoi tao cac bien ,nhung chua gan vao app
 db=SQLAlchemy()
 migrate=Migrate()
+login_manager=LoginManager()
 
 #dung ham _> goi gon qua trinh tao web _> de dang test thu
 def create_app(config_class=Config):
@@ -13,14 +15,24 @@ def create_app(config_class=Config):
         app=Flask(__name__)
         app.config.from_object(config_class)
 
+        #gắn các công cụ vào app
         db.init_app(app)          
         migrate.init_app(app,db)
+        login_manager.init_app(app)
 
         from app.routes import bp as main_bp
         app.register_blueprint(main_bp)
 
+        from app.auth import bp as auth_bp #DANG KY BLUEPRINT CHO AUTH
+        app.register_blueprint(auth_bp,url_prefix='/auth')
+
         return app
 
 from app import models
+
+from app.models import User
+@login_manager.user_loader
+def load_user(user_id):
+        return User.query.get(int(user_id))
 
 
---
+++ app/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms.validators import DataRequired,Length,Email,EqualTo
+
+class LoginForm(FlaskForm):
+    email=StringField('email',validators=[DataRequired(),Email()])
+    password= PasswordField('Mật khẩu',validators=[DataRequired(),Length(min=6,max=100)])
+    submit=SubmitField('Đăng nhập')
+    pass
+
+class RegistrationForm(FlaskForm):
+    username=StringField('Tên đăng nhâp',validators=[DataRequired(),Length(max=30)])
+    email=StringField('Email',validators=[DataRequired(),Email()])
+    password= PasswordField('Mật khẩu',validators=[DataRequired(),Length(min=6,max=100)])
+    confirm_password=PasswordField('Xác nhận mật khẩu',validators=[DataRequired(),EqualTo('password')])
+    submit=SubmitField('Đăng ký')
+    pass
